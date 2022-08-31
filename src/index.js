a5ce1b7f949aa0cc06423ab2df9d5a213918b6ab
Joshua Nobleza <joshnobleza@bell.net>
2022-08-31 16:57:06 -0400

    Add tab-switching logic

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './style.css';
+import navBar from "./nav-bar";
+import homePage from "./home";
+import contactPage from './contact';
+import menuPage from './menu';
+
+const content = document.querySelector("#content");
+
+content.appendChild(navBar());
+content.appendChild(homePage());
+
+const homeLink = document.querySelector('.home-link');
+const contactLink = document.querySelector('.contact-link');
+const menuLink = document.querySelector('.menu-link');
+
+homeLink.addEventListener('click', () => {
+    const main = document.querySelector('main');
+    main.remove();
+
+    content.appendChild(homePage());
+});
+
+contactLink.addEventListener('click', () => {
+    const main = document.querySelector('main');
+    main.remove();
+
+    content.appendChild(contactPage());
+});
+
+menuLink.addEventListener('click', () => {
+    const main = document.querySelector('main');
+    main.remove();
+
+    content.appendChild(menuPage());
+});
